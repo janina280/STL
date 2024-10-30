257f65e256b13f01c6e3db7c6a7bc09eac1e759f
janina280 <coceijanina@yahoo.com>
2024-10-30 09:44:15 +0200

    Resolve first problem - initial hands-on implementation

--- main.cpp
+++ main.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <fstream>
 
 using namespace std;
 
-int main()
-{
+struct Problem {
+    string name;
+    string specialty;
+};
+
+struct Doctor {
+    string id;
+    string specialty;
+};
+
+Problem currentProblem;
+
+bool isSpecialist(const Doctor& doc) {
+    return doc.specialty == currentProblem.specialty;
+}
+
+int main() {
     ifstream inFile("input.txt");
 
-    int no_problems, no_doctors;
-    string name, speciality;
-    
-    inFile >> no_problems;
+    int numProblems, numDoctors;
+    inFile >> numProblems;
 
-    for (int i = 0; i < no_problems; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    vector<Problem> problems(numProblems);
+    for (Problem& c : problems) {
+        inFile >> c.name >> c.specialty;
     }
 
-    inFile >> no_doctors;
-
-    for (int i = 0; i < no_doctors; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    inFile >> numDoctors;
+    vector<Doctor> doctors(numDoctors);
+    for (Doctor& d : doctors) {
+        inFile >> d.id >> d.specialty;
     }
+
+    for (const Problem& p : problems) {
+        currentProblem = p;
+        auto docIt = find_if(doctors.begin(), doctors.end(), isSpecialist);
+
+        if (docIt != doctors.end()) {
+            cout << p.name << " " << "Acceptat" <<endl;
+            doctors.erase(docIt);
+        }
+        else
+        {
+            cout << p.name << " " << "Respins" << endl;
+        }
+        
+    }
+
+    inFile.close();
 
     return 0;
 }
