52ce0d948eafe1727ec7043c6286d095e7ab5d66
janina280 <coceijanina@yahoo.com>
2024-11-12 22:02:56 +0200

    Hands-on STL #2

--- main.cpp
+++ main.cpp
@@ -1,58 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <fstream>
 
 using namespace std;
 
 struct Problem {
-    string name;
-    string specialty;
+	string name;
+	string specialty;
 };
 
 struct Doctor {
-    string id;
-    string specialty;
+	string id;
+	string specialty;
 };
 
-Problem currentProblem;
-
 int main() {
-    ifstream inFile("input.txt");
+	ifstream inFile("input2.txt");
 
-    int no_Problems, no_Doctors;
-    inFile >> no_Problems;
+	int no_Problems, no_Doctors;
+	inFile >> no_Problems;
 
-    vector<Problem> problems(no_Problems);
-    for (Problem& p : problems) {
-        inFile >> p.name >> p.specialty;
-    }
+	vector<Problem> problems(no_Problems);
+	for (Problem& problem : problems) {
+		inFile >> problem.name >> problem.specialty;
+	}
 
-    inFile >> no_Doctors;
-    vector<Doctor> doctors(no_Doctors);
-    for (Doctor& d : doctors) {
-        inFile >> d.id >> d.specialty;
-    }
+	inFile >> no_Doctors;
+	vector<Doctor> doctors(no_Doctors);
+	for (Doctor& doctor : doctors) {
+		inFile >> doctor.id >> doctor.specialty;
+	}
 
-    for (const Problem& p : problems) {
-        currentProblem = p;
-        bool found = false;
+	for (const Doctor& doctor : doctors)
+	{
+		auto problem_it = find_if(problems.begin(), problems.end(), [&](const Problem& problem) {return doctor.specialty == problem.specialty; });
+		if (problem_it != problems.end())
+		{
+			cout << doctor.id << " " << problem_it->name << " " << '\n';
+			problems.erase(problem_it);
+		}
+	}
 
-        for (size_t i = 0; i < doctors.size(); ++i) {
-            if (doctors[i].specialty == currentProblem.specialty) {
-                cout << p.name << " " << "Acceptat" << endl;
-                doctors.erase(doctors.begin() + i);
-                found = true;
-                break;
-            }
-        }
+	inFile.close();
 
-        if (!found) {
-            cout << p.name << " " << "Respins" << endl;
-        }
-    }
-
-    inFile.close();
-
-    return 0;
+	return 0;
 }
