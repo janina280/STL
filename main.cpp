1754a9a7a7016e38d0928631a547040f2c5dd713
janina280 <coceijanina@yahoo.com>
2024-12-13 17:00:02 +0200

    HandsOn STL #4

--- main.cpp
+++ main.cpp
@@ -3,47 +3,117 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include <map>
+#include <queue>
+#include <set>
 
 using namespace std;
 
+static const int open = 9;
+static const int close = 17;
+
 struct Problem {
-	string name;
-	string specialty;
+    string name;
+    string specialty;
+    int arrivalHour;
+    int severity;
+    int duration;
+
+    bool operator< (const Problem& other) const
+    {
+        if (arrivalHour != other.arrivalHour)
+            return  (arrivalHour > other.arrivalHour);
+        if (severity != other.severity)
+            return (severity < other.severity);
+
+        return (duration > other.duration);
+    }
 };
 
 struct Doctor {
-	string id;
-	string specialty;
+    string id;
+    set<string> specialities;
+    int available_time = 0;
+    vector<Problem> solvedProblems;
 };
 
-int main_excluded() {
-	ifstream inFile("input2.txt");
+int main() {
+    ifstream inFile("input2.txt");
 
-	int no_Problems, no_Doctors;
-	inFile >> no_Problems;
+    int no_Problems, no_Doctors;
+    inFile >> no_Problems;
 
-	vector<Problem> problems(no_Problems);
-	for (Problem& problem : problems) {
-		inFile >> problem.name >> problem.specialty;
-	}
+    priority_queue<Problem> problemsQueue;
+    for (int problemIdx = 0; problemIdx < no_Problems; problemIdx++)
+    {
+        Problem problem;
+        inFile >> problem.name >> problem.specialty >> problem.arrivalHour
+            >> problem.duration >> problem.severity;
+        problemsQueue.push(problem);
+    }
 
-	inFile >> no_Doctors;
-	vector<Doctor> doctors(no_Doctors);
-	for (Doctor& doctor : doctors) {
-		inFile >> doctor.id >> doctor.specialty;
-	}
 
-	for (const Doctor& doctor : doctors)
-	{
-		auto problem_it = find_if(problems.begin(), problems.end(), [&](const Problem& problem) {return doctor.specialty == problem.specialty; });
-		if (problem_it != problems.end())
-		{
-			cout << doctor.id << " " << problem_it->name << " " << '\n';
-			problems.erase(problem_it);
-		}
-	}
+    inFile >> no_Doctors;
 
-	inFile.close();
+    map<string, Doctor> doctorsById;
+    map<string, set<string>> doctorIdsBySpeciality;
+    for (int doctorIdx = 0; doctorIdx < no_Doctors; doctorIdx++)
+    {
+        Doctor doctor;
+        inFile >> doctor.id;
 
-	return 0;
+        int numSpecialities;
+        inFile >> numSpecialities;
+
+
+        for (int idx = 0; idx < numSpecialities; idx++)
+        {
+            std::string speciality;
+            inFile >> speciality;
+            doctor.specialities.insert(speciality);
+
+
+            doctorIdsBySpeciality[speciality].insert(doctor.id);
+        }
+
+        doctor.available_time = open;
+
+        doctorsById[doctor.id] = doctor;
+    }
+
+    while (!problemsQueue.empty())
+    {
+        Problem currentProblem = problemsQueue.top();
+        problemsQueue.pop();
+
+        const auto& doctorsIt = doctorIdsBySpeciality.find(currentProblem.specialty);
+        if (doctorsIt == doctorIdsBySpeciality.end())
+            continue;
+
+        const set<string>& doctorIdsSet = doctorsIt->second;
+        for (const string& doctorId : doctorIdsSet)
+        {
+            Doctor& doctor = doctorsById[doctorId];
+
+            if (doctor.available_time <= currentProblem.arrivalHour
+                && currentProblem.arrivalHour + currentProblem.duration <= close)
+            {
+                doctor.available_time = currentProblem.arrivalHour + currentProblem.duration;
+                doctor.solvedProblems.push_back(currentProblem);
+                break;
+            }
+        }
+    }
+    for (auto& doctorEntry : doctorsById)
+    {
+        Doctor& doctor = doctorEntry.second;
+        if (doctor.solvedProblems.size() > 0)
+        {
+            cout << doctor.id << " " << doctor.solvedProblems.size() << " ";
+            for (const Problem& problem : doctor.solvedProblems)
+                cout << problem.name << " " << problem.arrivalHour << " ";
+            cout << endl;
+        }
+    }
+    return 0;
 }
